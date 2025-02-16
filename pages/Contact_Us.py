62fec55b087e9f7152fefacd98fd536d3b77d9f5
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-02-16 12:33:04 +0530

    Add success/error confirmation message and rename page title

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import send_email
 
-st.set_page_config("center")
+st.set_page_config(page_title="Contact Us",layout="centered")
 st.header("Contact Us")
 with st.form(key="email_form"):
     emailId = st.text_input("Your Email Address : ",key="eid",)
     mess = st.text_area("Enter Your Message : ", key="message")
     button = st.form_submit_button("Submit")
     if button:
-        send_email.sendemail(mess,emailId)
-
+        if send_email.sendemail(mess,emailId):
+            st.success("Your message was sent successfully!",icon="✅")
+        else:
+            st.error("We ran into an error! Try again later.",icon="🚨")
