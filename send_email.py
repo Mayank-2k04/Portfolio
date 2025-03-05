6a49149a4bb83cd379f174fc192145f7dd30dbf9
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-03-05 23:32:54 +0530

    Minor fixes for deployment

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import streamlit as st
 host = "smtp.gmail.com"
 port = 465
 user = "pythonsendsmail8@gmail.com"
-pa = st.secrets("PYTHONEMAILPASS")  #environmental variable
+pa = st.secrets["PYTHONEMAILPASS"]  #environmental variable
 sslcontext = ssl.create_default_context()
 def sendemail(message,user_email):
     message = f"From: Mayank <{user}>\nTo: {user.split('@')[0]} <{user}>\nSubject: Message from {user_email}\nUser email: {user_email}\n{message}"
